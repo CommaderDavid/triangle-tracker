cd49d5e77dadd034f2296b2c5e41d4ae4c64af91
David Boedigheimer <stealth259@gmail.com>
2019-10-09 10:23:57 -0700

    Add back end and front end logic, and add number for input instead of text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,28 @@
+// Back end logic
+var triTracker = function(side1, side2, side3) {
+  if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+    return "NOT a triangle. Seriously, what is this"
+  } else if ((side1 === side2 && side1 != side3) || (side2 === side3 && side2 != side1)) {
+    return "Isosceles"
+  } else if (side1 != side2 && side1 != side3 && side2 != side3) {
+    return "Scalene"
+  } else if (side1 === side2 && side1 === side3) {
+    return "Equilateral"
+  }
+};
+
+// Front end logic
+
 $(document).ready(function() {
   $("form#math").submit(function(e) {
     var side1 = parseInt($("input#side1").val());
     var side2 = parseInt($("input#side2").val());
     var side3 = parseInt($("input#side3").val());
+    console.log(side1, side2, side3);
+    var result = triTracker(side1, side2, side3);
 
-    if (side1 === side2 && side1 === side3) {
-      var result = "Equilateral"
-
-      $("#tri").empty().append(result);
-      $("#hide").show();
-    } else if (side1 === side2 || side2 === side3) {
-      var result = "Isosceles"
-
-      $("#tri").empty().append(result);
-      $("#hide").show();
-    } else if (side1 != side2 && side1 != side3 && side2 != side3) {
-      var result = "Scalene"
-
-      $("#tri").empty().append(result);
-      $("#hide").show();
-    } else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
-      var result = "NOT a triangle. Seriously, what is this"
-
-      $("#tri").empty().append(result);
-      $("#hide").show();
-    } else {
-      alert("Please input three different numbers!");
-    }
+    $("#tri").empty().append(result);
+    $("#hide").show();
 
     e.preventDefault();
   });
